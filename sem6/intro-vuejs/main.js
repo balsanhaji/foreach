7f435f8ce127cc7cde3152f80c0f93d1fe7868bb
unknown <b.alsanhaji@gmail.com>
2020-01-06 13:49:05 +0100

    intro-vuejs

--- sem6/intro-vuejs/main.js
+++ sem6/intro-vuejs/main.js
@@ -1,3 +1,25 @@
-new Vue({
-	
+const app = new Vue({
+	el: '#app',
+	data: function() {
+		return {
+			titre: 'Mon application...',
+			mesClasses: 'gros-titre',
+			boutonDesactive: false,
+			sourceDeLimage:'https://www.uniconexed.org/wp-content/uploads/2017/05/canstockphoto75383057-happy-new-year-2020-770x544.jpg',
+			afficherMessage: true,
+			genre: ['femme', 'homme'],
+			formes: [
+				{id: 0, nom:'Pathé DIOP'},
+				{id: 1, nom:'Bilal AL SANHAJI'}
+			],
+			adresse: {
+				rue: 'Faubourg d\'Arras',
+				numero: 59,
+				ville: {
+					codePostal: 59000,
+					nom: 'Fâches-Thumesnil'
+				}
+			}
+		};
+	}
 })
